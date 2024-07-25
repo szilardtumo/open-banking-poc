52c8d87c7ec24e46a13911afcbdda1ddb185085e
Szilárd Tumó <szilard.tumo@airportlabs.com>
2024-07-25 16:59:37 +0200

    cleanup

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { helloRouter } from "./routers/post";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +7,7 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  hello: helloRouter,
 });
 
 // export type definition of API
